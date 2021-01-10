74b286f3cb257874cb8cb034d8e0a154ac8916aa
Savannah <2890513384@qq.com>
2021-01-10 20:12:14 +0800

    update random quote

--- random-quote/src/index.js
+++ random-quote/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; 
-import reportWebVitals from './reportWebVitals'; 
+import './index.css';  
 
 class RandomQuote extends React.Component{ 
   constructor(props){
@@ -71,7 +70,3 @@ class RandomQuote extends React.Component{
 
 ReactDOM.render( <RandomQuote />,  document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
